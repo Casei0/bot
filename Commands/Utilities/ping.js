13f4145d675f16b2e9b4f11b9927fa23ebc88eb3
Casei0 <96496013+Casei0@users.noreply.github.com>
2022-07-26 00:05:26 -0700

    Update ping.js

--- Commands/Utilities/ping.js
+++ Commands/Utilities/ping.js
@@ -4,17 +4,12 @@ const { Color, Green, Red, Yellow } = require("../../src/config.json");
 module.exports = {
     name: "ping",
     description: "Discord Bot Ping",
-    usage: "/ping",
     /**
      * 
      * @param {CommandInteraction} interaction 
      * @param {Client} client
      */
     async execute(interaction, client) {
-        let bws = client.guilds.cache.get("969358497923874816");
-        let clerityrole = bws.roles.cache.get("994834735526326333");
-        let member1 = bws.members.cache.get(interaction.user.id);
-
         const embed = new MessageEmbed()
             .setColor(Color)
             .setDescription(`**Ping:** \`${Date.now() - interaction.createdTimestamp}ms\``)
